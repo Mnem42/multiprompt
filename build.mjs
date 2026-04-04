4b9ad255a55c7fba0b75a2a440b9f4a23058c62b
mnem <mnem424242@gmail.com>
2026-04-04 20:48:28 +0100

    change to build script for my convenience
    # Conflicts:
    #	build.mjs

--- build.mjs
+++ build.mjs
@@ -1,5 +1,6 @@
 import * as esbuild from 'esbuild'
 import * as fs from 'fs'
+import { createServer } from "http";
 
 // Change this to whatever the mod should be called, without the extension.
 const MOD_NAME = "multiprompt"
@@ -27,10 +28,27 @@ await esbuild.build({
     banner: {
         js: `// ${MOD_NAME}.js`
     },
-}).then(async (result) =>{
+}).then(async () =>{
     let stats = fs.statSync(`sandboxels/mods/${MOD_NAME}.js`)
     console.log("Build finished")
     console.log("Build output size: ", stats.size, "(B)")
 
     fs.copyFile(`sandboxels/mods/${MOD_NAME}.js`, `sandboxels-mods/mods/${MOD_NAME}.js`, () => console.log("File copied"))
+
+    console.log("Faking live server...")
+
+    const data = fs.readFileSync(`sandboxels/mods/${MOD_NAME}.js`)
+
+    const hostname = 'localhost';
+    const port = 3000;
+
+    const server = createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/javascript');
+        res.end(data);
+    });
+
+    server.listen(port, hostname, () => {
+        console.log(`Mod hosted at: http://${hostname}:${port}/`);
+    });
 })
