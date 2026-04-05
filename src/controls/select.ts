a75ca5fc49ea560f1ba1a1661615487e3a811b30
mnem <mnem424242@gmail.com>
2026-04-05 15:35:01 +0100

    doc: general docs

--- src/controls/select.ts
+++ src/controls/select.ts
@@ -3,6 +3,10 @@ import { InputArgs } from "../core"
 type SelectOptions = { colour?: string }
 type SelectArgs<K, T> = [K, T, SelectOptions][]
 
+/**
+ * An input where you can select an input. If you're constructing one, you should probably use
+ * {@link SelectBuilder}
+ */
 export class SelectInput<K extends string, T> extends InputArgs<T, SelectArgs<K, T>> {
     public build(): HTMLElement {
         const container = document.createElement("div")
@@ -35,6 +39,7 @@ export class SelectInput<K extends string, T> extends InputArgs<T, SelectArgs<K,
     }
 }
 
+/** A builder for {@link SelectInput} */
 export class SelectBuilder<T, K extends string = never> {
     inputs: SelectArgs<K, T> = []
     default_key?: K = undefined
@@ -44,6 +49,13 @@ export class SelectBuilder<T, K extends string = never> {
         this.label = label
     }
 
+    /**
+     * Adds an option
+     *
+     * @param key The key to add
+     * @param value The value that maps to the key
+     * @param options The options for the input
+     */
     add_option<NK extends string>(key: NK, value: T, options?: SelectOptions):
         SelectBuilder<T, K | NK>
     {
@@ -51,11 +63,17 @@ export class SelectBuilder<T, K extends string = never> {
         return this
     }
 
+    /**
+     * Sets a given key as the default
+     *
+     * @param default_key The key to use as the default
+     */
     set_default(default_key: K): SelectBuilder<T, K> {
         this.default_key = default_key
         return this
     }
 
+    /** Builds this into a {@link SelectInput} */
     build(): SelectInput<K, T> {
         const default_v = this.inputs.find(x => x[0] == this.default_key)
 
@@ -67,8 +85,13 @@ export class SelectBuilder<T, K extends string = never> {
     }
 }
 
+/** A relative direction */
 type Direction = "up" | "down" | "left" | "right"
 
+/**
+ * A function to easily make a directional input. If you're using a {@link PromptBuilder}, you
+ * should use {@link PromptBuilder.dir_input}.
+ */
 export function dir_input(label: string): SelectInput<Direction, Direction> {
     const v = new SelectBuilder<Direction>(label)
 
