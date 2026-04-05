ff6bda4e1b3d00a8857cf34f1ce394f7fd326e17
mnem <mnem424242@gmail.com>
2026-04-05 15:28:04 +0100

    clean: remove unused alias

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,2 @@
 // ggod provided
 export type IsPartial<T> = { [K in keyof T]-?: object extends Pick<T, K> ? never : K }[keyof T]extends never ? true : false; 
-
-export type TrueStringLiterals<T extends string> = string extends T ? never : true;
