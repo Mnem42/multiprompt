feb69abe142860573f5a140e5147d5be939403d6
mnem <mnem424242@gmail.com>
2026-04-06 14:12:59 +0100

    feat: go back to initial script
    Since I don't want it full of stuff that only makes sense for my
    specific setup.

--- build.mjs
+++ build.mjs
@@ -1,9 +1,8 @@
 import * as esbuild from 'esbuild'
 import * as fs from 'fs'
-import { createServer } from "http";
 
 // Change this to whatever the mod should be called, without the extension.
-const MOD_NAME = "multiprompt"
+const MOD_NAME = "test"
 
 // Change this to something else if you want to make it go somewhere that it
 // normally wouldn't
@@ -16,7 +15,6 @@ await esbuild.build({
     platform: "browser",
     logLevel: "error",
     bundle: true,
-    // dropLabels: ["dbg_assert"],
 
     loader: {
         // Force .html, .svg, and .css to load as text
@@ -34,21 +32,4 @@ await esbuild.build({
     console.log("Build output size: ", stats.size, "(B)")
 
     fs.copyFile(`sandboxels/mods/${MOD_NAME}.js`, `sandboxels-mods/mods/${MOD_NAME}.js`, () => console.log("File copied"))
-
-    console.log("Faking live server...")
-
-    const data = fs.readFileSync(`sandboxels/mods/${MOD_NAME}.js`)
-
-    const hostname = 'localhost';
-    const port = 3000;
-
-    const server = createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/javascript');
-        res.end(data);
-    });
-
-    server.listen(port, hostname, () => {
-        console.log(`Mod hosted at: http://${hostname}:${port}/`);
-    });
 })
