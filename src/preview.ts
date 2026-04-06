b6706c42472a9c59bd23098abf5f89976f4a5657
mnem <mnem424242@gmail.com>
2026-04-06 15:26:23 +0100

    feat: settable preview canvas resolution

--- src/preview.ts
+++ src/preview.ts
@@ -16,20 +16,46 @@ export class CanvasPreview<K extends PropertyKey> extends ControlWithSubscriber<
     ctx: CanvasRenderingContext2D | null = null
     canvas: HTMLCanvasElement | null = null
 
+    width: number = 300
+    height: number = 150
+
     constructor(render: RenderCallback<K>) {
         super()
         this.render = render
     }
 
     build(): HTMLElement {
-        const canvas = document.createElement("canvas")
-        this.ctx = canvas.getContext("2d")
-        this.canvas = canvas
-        return canvas
+        this.canvas = document.createElement("canvas")
+        this.ctx = this.canvas.getContext("2d")
+
+        this.canvas.width = this.width
+        this.canvas.height = this.height
+
+        return this.canvas
     }
 
     subscriber(v: Map<K, unknown>): void {
         if (this.ctx !== null && this.canvas !== null)
             this.render(this.ctx, v, this.canvas)
+    }
+
+    /**
+     * Sets the canvas width and height attributes.
+     *
+     * Modifies the already built canvas if this is run after {@link CanvasPreview.build}
+     *
+     * @param width The new width
+     * @param height The new height
+     */
+    public with_resolution(width: number, height: number): this {
+        this.width = width
+        this.height = height
+
+        if (this.canvas !== null) {
+            this.canvas.width = width
+            this.canvas.height = height
+        }
+
+        return this
     }
 }
